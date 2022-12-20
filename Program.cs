eb7e7f3ae80320b660e9c7472271fa7b59645d97
christian <christianmorenodumit@gmail.com>
2022-12-20 13:05:48 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace firstappconsole
+{
+    internal class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Introduce Nombre: ");
+            string Nombre = Console.ReadLine();
+            Console.WriteLine("Holaaa, " + Nombre );
+
+            string time =  DateTime.Now.ToString("hh:mm:ss tt");
+            Console.WriteLine(time);
+        }
+        
+    }
+}
